ef5470c5be3892405ca055edca9329e5f58fa76b
Gaurang Naik <A00250808@mycambrian.ca>
2023-05-22 19:04:43 -0400

    Vowel identification

---
+++ Vowel/Vowel/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Vowel;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Vowel/Vowel/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Vowel;
+
+public partial class MainPage : ContentPage
+{
+	string[] vowels = { "a", "e", "i", "o", "u" };
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnClicked(object sender, EventArgs e)
+	{
+		string input = txtbxAlphabet.Text;
+		if (!string.IsNullOrEmpty(input))
+		{
+			if (vowels.Contains<string>(input.ToLower()))
+			{
+				lblOutput.Text = "This is a Vowel.";
+            }
+			else
+			{
+                lblOutput.Text = "This is not a Vowel.";
+            }
+		}
+		
+
+	}
+}
+
